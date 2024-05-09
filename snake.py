eb021484549a71ec0bd0757b77cf64b62c7c4e5c
M4uricio2002 <a01703337@tec.mx>
2024-05-08 18:05:37 -0600

    Validar snake.py com modulo flake8

--- snake.py
+++ snake.py
@@ -1,16 +1,6 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to mouse clicks.
-"""
-
 from random import randrange
-from turtle import *
-
+from turtle import (Screen, clear, done, hideturtle, listen, onkey, ontimer,
+                    tracer, update)
 from freegames import square, vector
 
 food = vector(0, 0)
@@ -58,7 +48,9 @@ def move():
     ontimer(move, 100)
 
 
-setup(420, 420, 370, 0)
+# Configuración de pantalla y controles
+screen = Screen()
+screen.setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
